4e621b377eeeed7664701afd7a7c5116b483f39b
Lara Filippone <lara.filippone@gmail.com>
2022-03-11 15:34:39 +0100

    JSNACK3 - created var arrayPiccolo

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -5,3 +5,7 @@ console.log('JS ok');
 
 // Creo un array con 10 numeri
 var arrayGrande=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Creo un array con meno numeri del primo array
+var arrayPiccolo=[1, 2, 3];
+
