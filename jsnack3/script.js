a405a4112c3ad58f9adb6dfc9407bcac94fd1234
Lara Filippone <lara.filippone@gmail.com>
2022-03-11 17:24:48 +0100

    JSNACK3 created for cycle that push numerber in arrayPiccolo while arrayPiccolo.length<arrayGrande.length

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -9,3 +9,9 @@ var arrayGrande=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Creo un array con meno numeri del primo array
 var arrayPiccolo=[1, 2, 3];
 
+// Creo un  ciclo for che inserisce un numero random finchè arrayPiccolo ha lo stesso  numero dell'arrayGrande
+for (var i=arrayPiccolo.length; arrayPiccolo.length < arrayGrande.length; i++){
+    arrayPiccolo.push(Math.floor((Math.random()*100)));
+}
+
+console.log(arrayPiccolo);
