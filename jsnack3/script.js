afd35802001170d0d5772ada5410273aa4652559
Lara Filippone <lara.filippone@gmail.com>
2022-03-11 15:30:29 +0100

    JSNACK3  INIT

---
+++ jsnack3/script.js
@@ -0,0 +1,4 @@
+/*Crea due array che hanno un numero di elementi diversi. Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.*/
+
+// Creo console.log per vedere se js file è collegatto a index.html
+console.log('JS ok');
