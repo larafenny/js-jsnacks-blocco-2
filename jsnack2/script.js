48e865a554c2623565070914657f14e71013428a
Lara Filippone <lara.filippone@gmail.com>
2022-03-11 15:04:13 +0100

    JSNACK2 - created var arrayDaRiempire

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,3 +1,7 @@
 /*Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.*/
 
 // Creo console.log per vedere se js file è collegatto a index.html
+console.log('JS ok');
+
+// Creo un array vuoto che successivamente verrà riempito
+let arrayDaRiempire=[];
