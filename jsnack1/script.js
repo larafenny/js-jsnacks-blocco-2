7489cd0cb5e9ae98452ae7a70acf5d76e6bc1922
Lara Filippone <lara.filippone@gmail.com>
2022-03-11 14:59:28 +0100

    JSNACK1 - created conditions to select the number in array in a odd number

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,7 +1,17 @@
 /*Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.*/
 
 // Creo un array con dalcuni numeri
-const array=[2, 1, 3, 1, 4, 2, 5];
+const array=[2, 1, 3, 1, 4, 10, 5];
 
 // Creo variabile somma inizializzandola a 0
 let somma=0;
+
+// Creo ciclo for per ispezionare ogni singolo elemento dell'array
+for(let i=0; i<array.length; i++){
+    //creo condizione che vede se l'indice dell'array è dispari e quando è così mi aggiorna la somma come somma di numeri in posizione dispari
+    if(i % 2 !==0){
+        somma = somma + array[i];
+    }
+}
+
+console.log(somma);
